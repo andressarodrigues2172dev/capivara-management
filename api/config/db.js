b67ec9ee746b0f938a4bea000a2a187cffde5e82
Andressa <andressa.rodrigues.2172@gmail.com>
2024-09-30 16:54:50 -0300

    "Atualiza configuração do banco de dados para usar DATABASE_URL"

--- api/config/db.js
+++ api/config/db.js
@@ -1,18 +1,17 @@
-require('dotenv').config(); // Certifique-se de que isso está no início do seu arquivo
+require('dotenv').config(); 
 
 const Sequelize = require('sequelize');
 
-// Configuração da conexão Sequelize usando variáveis de ambiente
-const sequelize = new Sequelize(
-  process.env.MYSQL_DATABASE,  // Nome do banco de dados
-  process.env.MYSQL_USERNAME,  // Nome de usuário MySQL
-  process.env.MYSQL_PASSWORD,   // Senha do MySQL
-  {
-    host: process.env.MYSQL_HOST,  // Host do MySQL
-    port: process.env.MYSQL_DOCKER_PORT, // Adicione esta linha
-    dialect: 'mysql',               // Dialeto (MySQL)
-  }
-);
+// Usando a URL completa do MySQL fornecida pelo Railway
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',               // Dialeto (MySQL)
+  dialectOptions: {
+    ssl: {
+      require: true, // Ative isso se o banco de dados exigir SSL (verifique a documentação do Railway)
+      rejectUnauthorized: false, // Permite certificados SSL auto-assinados
+    },
+  },
+});
 
 module.exports = sequelize;
 
